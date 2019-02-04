00ead7a5d5964d0909ecbd4dfd1ab62ac094bf13
AJITH KUMAR PANJA <ajithkumarpanja@gmail.com>
2019-02-04 18:06:25 +0530

    Updates

---
+++ precache-manifest.ea494249ec84d01634b54eb70eafc480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e8e928d1272ddf92c1d",
+    "url": "/OS_LAB_Project/static/css/main.3fc9c27f.chunk.css"
+  },
+  {
+    "revision": "2e8e928d1272ddf92c1d",
+    "url": "/OS_LAB_Project/static/js/main.2e8e928d.chunk.js"
+  },
+  {
+    "revision": "df7f83a6fd7a2512befb",
+    "url": "/OS_LAB_Project/static/js/1.df7f83a6.chunk.js"
+  },
+  {
+    "revision": "bd9f246863d46135e130",
+    "url": "/OS_LAB_Project/static/js/runtime~main.bd9f2468.js"
+  },
+  {
+    "revision": "49e92d20f9b2f384e805d60a337bc4b3",
+    "url": "/OS_LAB_Project/index.html"
+  }
+];
